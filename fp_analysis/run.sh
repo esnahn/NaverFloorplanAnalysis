3d4d87bdd692c52177f4ebdfefaae812d2ddacd9
Euisoon Ahn <euisoon.ahn@gmail.com>
2019-05-29 15:37:54 +0900

    docker image and run script

--- fp_analysis/run.sh
+++ fp_analysis/run.sh
@@ -1,9 +1,10 @@
-id=$( \
-docker run --runtime=nvidia -d -p 8888:8888 \
+echo "Removing container named fp"
+docker stop fp
+docker rm fp
+docker run --runtime=nvidia -d -p 8888:8888 --name fp \
 --mount source=data,target=/data \
 --mount type=bind,source=$HOME/fp_img,target=/fp_img,readonly \
---mount type=bind,source=$HOME/notebooks,target=/notebooks \
-fp_analysis \
-)
-sleep 10s
-docker exec $id jupyter notebook list
+--mount type=bind,source=$HOME/notebooks,target=/tf/notebooks \
+fp_analysis
+docker exec fp jupyter notebook list
+docker logs fp 2>&1 | grep "token="
