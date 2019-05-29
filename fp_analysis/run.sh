27cc671672282bdcd39eba81b57de7b654816c7e
Euisoon Ahn <euisoon.ahn@gmail.com>
2019-05-29 19:34:09 +0900

    added restart policy

--- fp_analysis/run.sh
+++ fp_analysis/run.sh
@@ -1,10 +1,17 @@
 echo "Removing container named fp"
 docker stop fp
 docker rm fp
+echo "Running the image"
 docker run --runtime=nvidia -d -p 8888:8888 --name fp \
 --mount source=data,target=/data \
 --mount type=bind,source=$HOME/fp_img,target=/fp_img,readonly \
 --mount type=bind,source=$HOME/notebooks,target=/tf/notebooks \
+--restart unless-stopped \
 fp_analysis
-docker exec fp jupyter notebook list
-docker logs fp 2>&1 | grep "token="
+
+
+echo "Waiting the server to run for 5 seconds..."
+sleep 5
+
+# docker logs fp 2>&1 | grep "token="
+docker exec fp jupyter notebook list | grep "token="
